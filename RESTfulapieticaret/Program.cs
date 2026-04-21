69f05f0d4bb163929b69290724d80905b6924dc9
Hamzaaladaadgithab <aladaada5105390@gmail.com>
2026-04-21 11:58:18 +0300

    Add project files.

---
+++ RESTfulapieticaret/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+//using Microsoft.EntityFrameworkCore;
+//using StudentManagementAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ربط قاعدة البيانات
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    //options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+
+// تفعيل Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
